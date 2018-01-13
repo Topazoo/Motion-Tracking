1a47926c053eb989242b7fecc78d0d76dbc60046
Peter Swanson <topazoo42@gmail.com>
2018-01-13 06:36:34 -0800

    Better multi-mouse async example

--- ex_multi_mouse_async.py
+++ ex_multi_mouse_async.py
@@ -20,15 +20,18 @@ device_1.connect()
 # Read all connected devices concurrently with devices labeled
 device_0.read_all(sync=False)
 
-# Loop reading 50 movements from device 1
+# Loop reading 50 movements from both devices
 x = 0
 while(x < 50):
-    movement = device_0.get_movement(label=True)
-    if(movement != None):
-        print movement
+    movement_0 = device_0.get_movement(label=True)
+    movement_1 = device_1.get_movement(label=True)
+    movement = (movement_0, movement_1)
+
+    if(movement != (None, None)):
+        print (movement_0, movement_1)
         x += 1
 
-# Loop reading 50 movements from device 2
+# Loop reading 50 movements from device 1
 x = 0
 while(x < 50):
     movement = device_1.get_movement(label=True)
